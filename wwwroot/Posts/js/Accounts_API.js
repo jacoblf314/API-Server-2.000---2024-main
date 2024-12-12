da26396dff64fee0d122cb3f5718ded7e3d19724
jacoblf314 <jacoblf314@gmail.com>
2024-12-12 10:01:58 -0500

    urls

--- wwwroot/Posts/js/Accounts_API.js
+++ wwwroot/Posts/js/Accounts_API.js
@@ -1,7 +1,7 @@
 class Accounts_API {
-    static API_URL() { return "http://localhost:5000/api/accounts"; }
-    static CONTROLLER_URL() { return "http://localhost:5000/accounts"; }
-    static LIKES_URL() { return "http://localhost:5000/likes"; }
+    static API_URL() { return "https://quick-sugared-wealth.glitch.me/api/accounts"; }
+    static CONTROLLER_URL() { return "https://quick-sugared-wealth.glitch.me/accounts"; }
+    static LIKES_URL() { return "https://quick-sugared-wealth.glitch.me/likes"; }
 
     static initHttpState() {
         this.currentHttpError = "";
